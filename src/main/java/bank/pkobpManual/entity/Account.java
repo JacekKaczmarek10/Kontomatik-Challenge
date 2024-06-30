a05923b720a5286eeb2cdb431b14fac1d6f718dc
DaniilKarunou <91436878+DaniilKarunou@users.noreply.github.com>
2024-06-30 10:06:58 +0200

    feat: add accounts info getter by session

---
+++ src/main/java/bank/pkobpManual/entity/Account.java
@@ -0,0 +1,19 @@
+package bank.pkobpManual.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Account {
+    private String name;
+    private AccountNumber accountNumber;
+    private String balance;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AccountNumber {
+        private String value;
+        private String format;
+    }
+}
