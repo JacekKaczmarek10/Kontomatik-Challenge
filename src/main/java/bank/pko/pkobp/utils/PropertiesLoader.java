8d81901e73e423345185f1bc648e1c66ed4b6810
JaKaczma <jacek.kaczmarek@rws.com>
2024-07-05 23:16:36 +0200

    refactor: remove unused import

--- src/main/java/bank/pko/pkobp/utils/PropertiesLoader.java
+++ src/main/java/bank/pko/pkobp/utils/PropertiesLoader.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
 
