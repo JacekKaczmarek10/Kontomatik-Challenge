1b906c2edd5dacea51d3a6c75f0bbb69d7e444f4
JaKaczma <jacek.kaczmarek@rws.com>
2024-07-06 10:59:43 +0200

    refactor: rename functions

--- src/main/java/bank/pkobp/PKOBPBankService.java
+++ src/main/java/bank/pkobp/PKOBPBankService.java
@@ -9,12 +9,7 @@ import static bank.pkobp.utils.PropertiesLoader.loadProperties;
 @Slf4j
 public class PKOBPBankService {
 
-    public static void main(String[] args) throws Exception {
-        PKOBPBankService pkobpBankService = new PKOBPBankService();
-        pkobpBankService.performLogin();
-    }
-
-    public void performLogin() throws Exception {
+    public void loginAndGetAccountData() throws Exception {
         UserCredentials userCredentials = loadProperties();
 
         if (userCredentials == null){
